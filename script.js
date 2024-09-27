8a16672acc465f8eb3192aff40f5c644adac2058
Bendo-Jr <danio.radaniel@student.passerellesnumeriques.org>
2024-09-27 17:54:02 +0300

    lien fo koa

--- script.js
+++ script.js
@@ -3,8 +3,3 @@ window.addEventListener("scroll", () =>{
     header.classList.toggle("sticky", window.scrollY > 0)
 })
 
-let fanazavana = document.getElementById('monButton');
-fanazavana.addEventListener('click', (event) => {
-    window.location.href = "https://www.meer.com/fr/21339-le-mariage-sakalava"
-    console.log("efa nikitika fanazavana aho")
-})
